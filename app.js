c0f15ce5922131d891fea52cd19d9cb910ef95f9
andfwas <andfwas@gmail.com>
2017-12-03 15:25:29 -0700

    we have clickable tones

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+
 var tones = document.getElementsByClassName('tone')
+
 var tonesListeners = function() {
   for (var i = 0; i < tones.length; i++) {
     let tone = tones[i]
     tone.addEventListener('click', () => {
-      console.log(tone.id);
+      console.log(tone);
+      let oscillator = audioCtx.createOscillator()
+      let gainNode = audioCtx.createGain()
+      oscillator.type = "square"
+      oscillator.frequency.value = 200
+      oscillator.attackTime = 0.1
+      oscillator.decayTime = 0.5
+      oscillator.releaseTime = 0.5
+      gainNode.gain.value = 0.1
+      oscillator.connect(gainNode)
+      gainNode.connect(audioCtx.destination)
+      oscillator.start();
+      oscillator.stop(audioCtx.currentTime + 0.5)
+
     })
   }
 }
 tonesListeners()
+
+// function playTone(color) {
+//   if (color) {
+//     console.log(color);
+//
+//   }
+// }
--- tones.js
+++ tones.js
@@ -1,12 +1,19 @@
 console.log('tones test')
 
-var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
-var oscillator = audioCtx.createOscillator()
-var gainNode = audioCtx.createGain()
-
-oscillator.type = "square"
-oscillator.frequency.value = 200
-gainNode.gain.value = 0.5
-
-oscillator.connect(gainNode)
-gainNode.connect(audioCtx.destination)
+// var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+// var oscillator = audioCtx.createOscillator()
+// var gainNode = audioCtx.createGain()
+//
+// oscillator.type = "square"
+// oscillator.frequency.value = 200
+// oscillator.attackTime = 0.1
+// oscillator.decayTime = 0.5
+// oscillator.releaseTime = 0.5
+// gainNode.gain.value = 0.1
+//
+// oscillator.connect(gainNode)
+// gainNode.connect(audioCtx.destination)
+//
+// oscillator.start();
+//
+// oscillator.stop(audioCtx.currentTime + 2)
