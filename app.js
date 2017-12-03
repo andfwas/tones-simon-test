2c3b3efe2d30c50b9b7d0e402880852d69659bdb
andfwas <andfwas@gmail.com>
2017-12-03 10:28:20 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,10 @@
+var tones = document.getElementsByClassName('tone')
+var tonesListeners = function() {
+  for (var i = 0; i < tones.length; i++) {
+    let tone = tones[i]
+    tone.addEventListener('click', () => {
+      console.log(tone);
+    })
+  }
+}
+tonesListeners()
