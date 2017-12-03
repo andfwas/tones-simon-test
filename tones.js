f6b72d85960c75bfee264d60ac8d14648c6933f1
andfwas <andfwas@gmail.com>
2017-12-03 14:45:38 -0700

    added tones.js

--- tones.js
+++ tones.js
@@ -1,1 +1,12 @@
 console.log('tones test')
+
+var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+var oscillator = audioCtx.createOscillator()
+var gainNode = audioCtx.createGain()
+
+oscillator.type = "square"
+oscillator.frequency.value = 200
+gainNode.gain.value = 0.5
+
+oscillator.connect(gainNode)
+gainNode.connect(audioCtx.destination)
