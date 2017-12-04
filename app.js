b069f847c8c6714642cac4e23eacc15fd2649629
andfwas <andfwas@gmail.com>
2017-12-03 19:48:10 -0700

    added depth to oscillators with for if else loops

--- app.js
+++ app.js
@@ -14,22 +14,31 @@ tonesListeners()
 
 function playTone(color) {
   if (color) {
-    var decay = decay
-    let frequency = color.id.length*40
-    let oscillator = audioCtx.createOscillator()
-    let gainNode = audioCtx.createGain()
-    // let time = audioCtx.currentTime
-    oscillator.type = "square"
-    oscillator.frequency.value = frequency
-    oscillator.attackTime = 0.01
-    oscillator.decayTime = 0.2
-    oscillator.releaseTime = 0.5
-    gainNode.gain.value = 0.1
-    oscillator.connect(gainNode)
-    gainNode.connect(audioCtx.destination)
-    oscillator.start();
-    oscillator.stop(audioCtx.currentTime + 0.4)
-    console.log(audioCtx.currentTime)
+    for (var i = 0; i < 4; i++) {
+      let frequency = color.id.length*40
+      let oscillator = audioCtx.createOscillator()
+      let gainNode = audioCtx.createGain()
+      let time = audioCtx.currentTime
+      if (i == 0) {
+        oscillator.type = "square"
+      } else if (i == 1) {
+        oscillator.type = "sawtooth"
+      } else if (i ==2) {
+        oscillator.type = "triangle"
+      } else {
+        oscillator.type = "sine"
+      }
+      oscillator.frequency.value = frequency
+      oscillator.attackTime = 0.01
 
+      oscillator.releaseTime = 0.5
+      gainNode.gain.value = 0.3
+      oscillator.connect(gainNode)
+      gainNode.connect(audioCtx.destination)
+      oscillator.start();
+      gainNode.gain.linearRampToValueAtTime(0.0, time + 0.4);
+      oscillator.stop(audioCtx.currentTime + 0.4)
+      console.log(audioCtx.currentTime)
+    }
   }
 }
