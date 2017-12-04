d0d1a99d453960691a0caf1d2a01d741f3ccdf9b
andfwas <andfwas@gmail.com>
2017-12-03 22:59:08 -0700

    tones are chords

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 var audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+// var distortion = audioCtx.createWaveShaper()
 
 var tones = document.getElementsByClassName('tone')
 
 var tonesListeners = function() {
   for (var i = 0; i < tones.length; i++) {
-    let tone = tones[i]
-    tone.addEventListener('click', () => {
-      playTone(tone)
+    let color = tones[i]
+    color.addEventListener('click', () => {
+      playTone(color)
     })
   }
 }
@@ -15,21 +16,33 @@ tonesListeners()
 function playTone(color) {
   if (color) {
     for (var i = 0; i < 4; i++) {
-      let oscillator = audioCtx.createOscillator()
-      let gainNode = audioCtx.createGain()
       let time = audioCtx.currentTime
-      let frequency  =color.id.length*30 - i*0.15
-      oscillator.type = "sawtooth"
-      oscillator.frequency.value = frequency
-      oscillator.attackTime = 0.01
-
-      oscillator.releaseTime = 0.5
-      gainNode.gain.value = 0.4
-      oscillator.connect(gainNode)
+      let gainNode = audioCtx.createGain()
+      gainNode.gain.value = 0
       gainNode.connect(audioCtx.destination)
+
+      let oscillator = audioCtx.createOscillator()
+      oscillator.connect(gainNode)
+      oscillator.type = "sawtooth"
+      oscillator.attackTime = 0.01
+      oscillator.releaseTime = 0.39
+
+      let frequency = color.id.length*Math.PI*30 - Math.PI*i*15
+      oscillator.frequency.value = frequency
+
+      // make a sound
       oscillator.start();
-      gainNode.gain.linearRampToValueAtTime(0.0, time + 0.4);
-      oscillator.stop(audioCtx.currentTime + 0.4)
+      console.log(time);
+      // attack
+      gainNode.gain.linearRampToValueAtTime(1.0, time + 0.05);
+      // decay
+      gainNode.gain.exponentialRampToValueAtTime(0.2, time + 0.1);
+      // sustain
+      gainNode.gain.linearRampToValueAtTime(1.0, time + 0.2);
+      // release
+      gainNode.gain.exponentialRampToValueAtTime(0.2, time + 0.2);
+      // stop
+      oscillator.stop(time + 0.3)
     }
   }
 }
